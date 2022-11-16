c26fe424fa141361c673c84708adc8be042b378b
Richardjackson-web <richardjackson556@gmail>
2022-11-16 12:49:29 +0100

    Creation: I created the different component folders and their respectives jsx files and scss files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "bootstrap/scss/bootstrap.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./styles/custom.style.scss";
+import AppRoutes from "./pages/routes";
+
+const App = () => {
+  return <AppRoutes />;
+};
+
+export default App;
